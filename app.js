27b5b010bffae74b16e7cc176fdb867bd8bdc8a2
Danil Rybkin <danil.rybkin.19@gmail.com>
2023-03-03 12:51:11 +0200

    deploy

--- app.js
+++ app.js
@@ -10,13 +10,11 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/cities', require('./routes/city.routes'));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
 
 const PORT = process.env.PORT || 5000;
 
